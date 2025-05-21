e18d0fc56f2df162e62d3707b752025798ee4977
ZardsMK <161655537+ZardsMK@users.noreply.github.com>
2025-05-20 23:57:58 -0300

    Update Historico.java

--- Historico.java
+++ Historico.java
@@ -1,14 +1,29 @@
+import java.util.ArrayList;
+
 public class Historico {
+    ArrayList<String> bancoR = new ArrayList<String>();
+    ArrayList<String> bancoV = new ArrayList<String>();
     
     public Historico() {
-        
     }
     
-    public void AtualizarBanco() {
-        
+    public void atualizarVenda(String msg) {
+        bancoV.add(msg);
     }
     
-    public void FazerRelatorio() {
-        
+    public void atualizarReceita(String msg) {
+        bancoR.add(msg);
+    }
+    
+    public void mostrarReceitas() {
+        for(String i : bancoR) {
+            System.out.println(i);
+        }
+    }
+    
+    public void mostrarVendas() {
+        for(String i : bancoV) {
+            System.out.println(i);
+        }
     }
 }
